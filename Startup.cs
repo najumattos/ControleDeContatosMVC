38887fa0f5f13430dec146e8aed89aa425622041
Ana Mattos <anajuliamattos02@gmail.com>
2025-08-25 16:40:23 -0300

    iniciando banco de dados

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using CadastroDeContatosMVC.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -23,6 +24,7 @@ namespace CadastroDeContatosMVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddEntityFrameworkSqlServer().AddDbContext<BancoContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
